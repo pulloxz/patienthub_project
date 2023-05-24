6d3d33d9cfd025f4d53defc7832c6db45f812c24
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2023-05-24 21:18:40 +0300

    updating booking page

--- src/pages/bookingPage.jsx
+++ src/pages/bookingPage.jsx
@@ -9,7 +9,8 @@ const BookingPage = () => {
   const selectedService = "الخدمة المختارة"; // Has to be replace with the dynamic value from the previous page
 
   return (
-    <nav className="navbar">
+    <div>
+      <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-left">
           <button
@@ -29,7 +30,7 @@ const BookingPage = () => {
             }}
           >
             <IoIosArrowBack style={{ width: '11px', height: '20px', marginRight: '5px' }} />
-            <span style={{ fontSize: '36px', marginLeft: '5px', stroke: '0.5px' }}>رجوع</span>
+            <span style={{ fontSize: '24px', marginLeft: '5px', stroke: '0.5px' }}>رجوع</span>
           </button>
         </div>
         <div
@@ -37,22 +38,27 @@ const BookingPage = () => {
           style={{
             position: 'absolute',
             top: '85px',
-            right: '85px',
+            right: '40px',
             color: '#4E4766',
-            fontSize: '36px',
+            fontSize: '32px',
             stroke: '0.5px',
           }}
         >
           بيشنت هاب
         </div>
-        <div
+        
+        
+      </div>
+    </nav>
+    <div className='page'>
+<div
           className="selected-service"
           style={{
             position: 'absolute',
-            top: '335px',
-            right: '85px',
+            top: '320px',
+            right: '40px',
             color: '#4E4766',
-            fontSize: '36px',
+            fontSize: '25px',
             stroke: '1.0px',
           }}
         >
@@ -62,16 +68,56 @@ const BookingPage = () => {
           className="additional-text"
           style={{
             position: 'absolute',
-            top: '385px',
-            right: '85px',
+            top: '350px',
+            right: '40px',
             color: '#4E4766',
-            fontSize: '24px',
+            fontSize: '18px',
           }}
         >
          قم بالتأكد من التواريخ المتاحة و احجز الموعد و الوقت الذي يناسبك
         </div>
-      </div>
-    </nav>
+        <hr
+          className="divider"
+          style={{
+            position: 'absolute',
+            top: '380px',
+            right: '40px',
+            width: '600px',
+            borderTop: '1px solid #4E4766',
+          }}
+        />
+
+
+<div
+          className="selected-service"
+          style={{
+            position: 'absolute',
+            top: '340px',
+            right: '700px',
+            color: '#4E4766',
+            fontSize: '25px',
+            stroke: '1.0px',
+          }}
+        >
+         تفاصيل الخدمة
+        </div>
+        
+
+      <hr
+          className="divider"
+          style={{
+            position: 'absolute',
+            top: '380px',
+            right: '700px',
+            width: '200px',
+            borderTop: '1px solid #4E4766',
+          }}
+        />
+        
+
+    </div>
+    </div>
+    
   );
 };
 
