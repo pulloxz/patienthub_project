4d379cb89724ca64c48cc26a2c3f9711deff9925
nawalhaider1 <109910086+nawalhaider1@users.noreply.github.com>
2023-05-30 17:53:54 +0300

    homepage

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import './App.css';
 import Navbar from './components/navbar';
 import HomePage from './pages/HomePage';
 import './pages/HomePage1.css'
-import { Height } from '@mui/icons-material';
+
 
 function App() {
   return (
   
     <div className="App">
-      <div style={{
-        width: '150px',
-        Height: '150px',
-        backgroundImage: `url(${require('./image/pic1.jpg')})`
-      }}>
-
-      </div>
+     
       <HomePage></HomePage>
       
     </div>
