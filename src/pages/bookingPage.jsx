2a077a30edc2b99430cba6be6c11d67cdd35ef97
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2023-06-30 00:25:34 +0300

    user-info-conformation_page

--- src/pages/bookingPage.jsx
+++ src/pages/bookingPage.jsx
@@ -4,8 +4,7 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
-import { availableDates } from "./availabledates";
-import { useLocation , useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 import './bookingpage.css';
@@ -13,7 +12,6 @@ import './bookingpage.css';
 const BookingPage = () => {
   const [selectedDate, setPickedDate] = useState(null);
   const currentDate = dayjs().startOf('day');
-  const location = useLocation();
   const navigate = useNavigate();
   const handleDateChange = (date) => {
     const selectedDayOfWeek = dayjs(date).day();
@@ -25,8 +23,24 @@ const BookingPage = () => {
       return;
     }
   
-    setPickedDate(date);
+    setPickedDate(date.toDate());
   };
+
+  const startDate = dayjs('2023-07-01');
+  const endDate = dayjs('2024-07-1');
+  const availableDates = generateDateRange(startDate, endDate);
+
+  function generateDateRange(start, end) {
+    const dates = [];
+    let currentDate = start;
+
+    while (currentDate.isBefore(end)) {
+      dates.push(currentDate.toISOString().substr(0, 10));
+      currentDate = currentDate.add(1, 'day');
+    }
+
+    return dates;
+  }
   
 
  const handleBackClick = () => {
@@ -41,10 +55,16 @@ navigate('/');
 
 const handleBookingConfirmation = () => {
     if (selectedTime && selectedDate) {
-      // Perform booking confirmation logic
       console.log('Booking confirmed!');
-      // Navigate to UserInfoConfirmation page
-      navigate('/userinfoconf');
+      navigate('/booking/confirmation',
+      {
+        state: {
+          selectedService: selectedService,
+          selectedDate: selectedDate,
+          selectedTime: selectedTime
+        }
+      }
+    ); 
     } else {
       console.log('Please select a date and time.');
     }
@@ -56,7 +76,7 @@ const handleBookingConfirmation = () => {
   const minSelectableDate = currentDate.add(1, 'day').startOf('day'); 
   const isBookingAvailable = isDateAvailable && selectedTime;
 return (
-    <div>
+    <div className="page">
       <nav className="navbar">
         <div className="navbar-container">
           <div className="navbar-left">
@@ -88,7 +108,6 @@ return (
               right: '40px',
               color: '#4E4766',
               fontSize: '32px',
-              stroke: '0.5px',
             }}
           >
             بيشنت هاب
@@ -242,6 +261,7 @@ return (
           )}
         </div>
         {isBookingAvailable && (
+
           <div className="booking-confirmation">
 
             <button type="button" className="t4" onClick={handleBookingConfirmation}>
@@ -264,262 +284,3 @@ const CustomInput = ({ value, onClick }) => (
 
 export default BookingPage;
 
-
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-// import { IoIosArrowBack } from 'react-icons/io';
-// import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-// import React, { useState } from 'react';
-// import dayjs from 'dayjs';
-// import { availableDates } from "./availabledates";
-// import './bookingpage.css';
-
-// const BookingPage = () => {
-//   const [selectedDate, setPickedDate] = useState(null);
-//   // const availableDates = 
-//   //  ['2023-06-23', '2023-06-25', '2023-06-28'];
-//   const currentDate = dayjs().startOf('day');
-
-//   const handleDateChange = (date) => {
-//     setPickedDate(date);
-//   };
-
-//   const handleBackClick = () => {
-    
-//   };
-//   const availableTimes = ['10:00 AM', '12:00 PM', '3:00 PM', '5:00 PM'];
-//   const [selectedTime, setSelectedTime] = useState('');
-
-//   const handleTimeClick = (time) => {
-//     setSelectedTime(time);
-//   };
-
-
-//   const selectedService = "الخدمة المختارة"; // Replace with the dynamic value from the previous page
-//   const isDateAvailable = availableDates.includes(selectedDate?.toISOString().substr(0, 10));
-//   const minSelectableDate = currentDate.add(1, 'day').startOf('day'); 
-
-//   return (
-//     <div>
-//       <nav className="navbar">
-//         <div className="navbar-container">
-//           <div className="navbar-left">
-//             <button
-//               className="back-button"
-//               onClick={handleBackClick}
-//               style={{
-//                 background: 'transparent',
-//                 border: 'none',
-//                 position: 'absolute',
-//                 left: '95px',
-//                 top: '85px',
-//                 display: 'flex',
-//                 alignItems: 'center',
-//                 color: '#4E4766',
-//                 stroke: '0.5px',
-//                 fontSize: '20px',
-//               }}
-//             >
-//               <IoIosArrowBack style={{ width: '11px', height: '20px', marginRight: '5px' }} />
-//               <span style={{ fontSize: '24px', marginLeft: '5px', stroke: '0.5px' }}>رجوع</span>
-//             </button>
-//           </div>
-//           <div
-//             className="navbar-right"
-//             style={{
-//               position: 'absolute',
-//               top: '85px',
-//               right: '40px',
-//               color: '#4E4766',
-//               fontSize: '32px',
-//               stroke: '0.5px',
-//             }}
-//           >
-//             بيشنت هاب
-//           </div>
-//         </div>
-//       </nav>
-//       <div className="page">
-//         <div
-//           className="selected-service"
-//           style={{
-//             position: 'absolute',
-//             top: '320px',
-//             right: '40px',
-//             color: '#4E4766',
-//             fontSize: '25px',
-//             stroke: '1.0px',
-//           }}
-//         >
-//           {selectedService}
-//         </div>
-//         <div
-//           className="additional-text"
-//           style={{
-//             position: 'absolute',
-//             top: '350px',
-//             right: '40px',
-//             color: '#4E4766',
-//             fontSize: '18px',
-//           }}
-//         >
-//           قم بالتأكد من التواريخ المتاحة و احجز الموعد و الوقت الذي يناسبك
-//         </div>
-//         <hr
-//           className="divider"
-//           style={{
-//             position: 'absolute',
-//             top: '380px',
-//             right: '40px',
-//             width: '600px',
-//             borderTop: '1px solid #4E4766',
-//           }}
-//         />
-//         <div
-//           className="selected-service"
-//           style={{
-//             position: 'absolute',
-//             top: '340px',
-//             right: '700px',
-//             color: '#4E4766',
-//             fontSize: '25px',
-//             stroke: '1.0px',
-//           }}
-//         >
-//           تفاصيل الخدمة: {selectedService}
-//         </div>
-//         <hr
-//           className="divider"
-//           style={{
-//             position: 'absolute',
-//             top: '380px',
-//             right: '700px',
-//             width: '300px',
-//             borderTop: '1px solid #4E4766',
-//           }}
-//         />
-        
-//         {isDateAvailable && selectedDate && (
-//   <div
-//     className="selected-date"
-//     style={{
-//       position: 'absolute',
-//       top: '410px',
-//       left: '380px',
-//       color: '#4E4766',
-//       fontSize: '18px',
-//     }}
-//   >
-//     تاريخ الحجز: {dayjs(selectedDate).format('YYYY-MM-DD')}
-//   </div>
-// )}
-// {isDateAvailable && selectedDate && (
-//   <div className="booking-field" style={{  
-//     position: 'absolute',
-//   top: '430px',
-//   left: '380px',
-//   marginTop: '10px',
-//   color: '#4E4766',
-//   fontSize: '18px', }}>
-//     <label className="booking-label">وقت الحجز:</label>
-//     <div className="selected-time">
-//       {selectedTime ? selectedTime : 'Not selected'}
-//     </div>
-//   </div>
-// )}
-
-
-
-//         {/* {isDateAvailable && selectedDate && (
-//           <div
-//             className="selected-date"
-//             style={{
-//               position: 'absolute',
-//               top: '410px',
-//               left: '380px',
-//               color: '#4E4766',
-//               fontSize: '18px',
-//             }}
-//           >
-//                 تاريخ الحجز: {dayjs(selectedDate).format('YYYY-MM-DD')}
-
-//           </div>
-
-//         )}
-//         <div className="booking-field">
-//           <label className="booking-label">وقت الحجز:</label>
-//           <div className="selected-time">
-//             {selectedTime ? selectedTime : 'Not selected'}
-//           </div>
-//         </div> */}
-//         <div className="available-times">
-//         <div className="time-slots">
-//           {availableTimes.map((time, index) => (
-//             <div
-//               key={time}
-//               className={`time-slot ${selectedTime === time ? 'selected' : ''}`}
-//               style={{
-//                 top: Math.floor(index / 2) * 50 + 420,
-//           left: (index % 2) * 150 + 800,
-//               }}
-//               onClick={() => handleTimeClick(time)}
-//             >
-//               {time}
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-
-
-//         <div
-//           className="calendar-container"
-//           style={{
-//             position: 'absolute',
-//             top: '400px',
-//             right: '40px',
-//             width: '200px',
-//             height: '100px',
-//             background: 'white',
-//             color: '#4E4766',
-//             display: 'flex',
-//             justifyContent: 'center',
-//             alignItems: 'center',
-//             textAlign: 'center',
-//           }}
-//         >
-//           <LocalizationProvider dateAdapter={AdapterDayjs}>
-//             <DatePicker
-//               value={selectedDate}
-//               onChange={handleDateChange}
-//               minDate={minSelectableDate}
-//               renderInput={(params) => <CustomInput {...params} />}
-           
-//                    />
-//           </LocalizationProvider>
-//           {!isDateAvailable && selectedDate && (
-//             <div className="error-message" style={{ 
-//               // marginTop: '10px',
-//               //  color: 'red' 
-//               position:'absolute',
-//               top:'100%',
-//               Padding:'10px',
-//               color:'red',
-
-              
-//                }}>
-//              هذا الموعد غير متاح قم بالتأكد من التواريخ المتاحة الاخرى
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const CustomInput = ({ value, onClick }) => (
-//   <button className="custom-input" onClick={onClick}>
-//     {value || 'Select Date'}
-//   </button>
-// );
-
-// export default BookingPage;
--- src/pages/userinfoconf.jsx
+++ src/pages/userinfoconf.jsx
@@ -1,15 +1,226 @@
-import React from "react";
+import './userinfoconf.css'
+import React, { useState } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { IoIosArrowBack } from 'react-icons/io';
+import { useLocation, useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import dayjs from 'dayjs';
 
-const Userinfoconformation = () => {
+const validationSchema = Yup.object({
+  الاسم: Yup.string().min(2, 'الاسم يجب ان يتكون من حرفين على الاقل').required('الاسم مطلوب'),
+  اسم_الاب: Yup.string().min(2, 'الاسم يجب ان يتكون من حرفين على الاقل').required('الاسم مطلوب'),
+  العمر: Yup.number().min(5, 'لا يمكن أن يكون العمر أقل من 5 سنوات').required('العمر مطلوب'),
+  العنوان: Yup.string().required('العنوان مطلوب'),
+  رقم_الهاتف: Yup.string().matches(/^\d+$/, 'يجب أن يحتوي رقم الهاتف على أرقام فقط').required('رقم الهاتف مطلوب'),
+});
+
+
+const UserInfoConformation = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [bookingSuccess, setBookingSuccess] = useState(false);
+  const selectedService = location.state?.selectedService || 'الخدمة المختارة';
+  const selectedDate = dayjs(location.state?.selectedDate).format('YYYY-MM-DD');
+  const selectedTime = location.state?.selectedTime?.toString();
+  const initialValues = {
+    الاسم: '',
+    اسم_الاب: '',
+    العنوان: '',
+    رقم_الهاتف: '',
+    العمر:'',
+  };
+
+  
+  const handleSubmit = () => {
+    
+    setBookingSuccess(true);
+    setTimeout(() => {
+      navigate('/booking');
+    }, 10000); 
+
+   
+  };
+
+  const handleBackClick = () => {
+    navigate('/booking');
+  };
+
   return (
     <div>
-        <h1
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-left">
+            <button
+              className="back-button"
+              onClick={handleBackClick}
+              style={{
+                background: 'transparent',
+                border: 'none',
+                position: 'absolute',
+                left: '95px',
+                top: '85px',
+                display: 'flex',
+                alignItems: 'center',
+                color: '#4E4766',
+                fontSize: '20px',
+              }}
+            >
+              <IoIosArrowBack style={{ width: '11px', height: '20px', marginRight: '5px' }} />
+              <span style={{ fontSize: '24px', marginLeft: '5px', stroke: '0.5px' }}>رجوع</span>
+            </button>
+          </div>
+          <div
+            className="navbar-title"
+            style={{
+              position: 'absolute',
+              top: '85px',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              color: '#4E4766',
+              fontSize: '32px',
+            
+            }}
+          >
+            تأكيد الحجز
+          </div>
+          <div
+            className="navbar-right"
+            style={{
+              position: 'absolute',
+              top: '85px',
+              right: '40px',
+              color: '#4E4766',
+              fontSize: '32px',
+            }}
+          >
+            بيشنت هاب
+          </div>
+        </div>
+      </nav>
+      <div
+        className="selected-service"
         style={{
-            color:'black'
+          position: 'absolute',
+          top: '200px',
+          right: '40px',
+          color: '#4E4766',
+          fontSize: '25px',
+          padding: '5px',
         }}
-        >new page</h1 >
+      >
+        {selectedService}
+      </div>
+      <div
+        className="selected-time"
+        style={{
+          position: 'absolute',
+          top: '240px',
+          right: '40px',
+          color: '#4E4766',
+          fontSize: '20px',
+        }}
+      >
+    تاريخ الحجز:- {dayjs(selectedDate).format('YYYY-MM-DD')}
+      </div>
+      <div
+        className="selected-time"
+        style={{
+          position: 'absolute',
+          top: '270px',
+          right: '40px',
+          color: '#4E4766',
+          fontSize: '20px',
+        }}
+      >
+        وقت الحجز: {selectedTime}
+      </div>
+      <div className="userinfo-confirmation">
+      <h2
+      style={{
+        fontSize: 20,
+      }}
+      >الرجاء اعطاء المعلومات التالية</h2>
+      {bookingSuccess ? (
+        <div className="success-message"
+        style={{
+          fontSize:20,
+        }}
+        >
+          تم الحجز بنجاح
+          <br />
+          جاري اعادة التوجيه إلى صفحة الحجز
+        </div>
+      ) : (
+ <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+        <Form>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="الاسم" style={{ fontSize: 20 }}>الاسم</label>
+              <Field
+                type="text"
+                name="الاسم"
+                className="form-control"
+                required
+              />
+              <ErrorMessage name="الاسم" component="div" className="error-message" style={{ fontSize: 20 }} />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="اسم_الاب" style={{ fontSize: 20 }}>اسم الاب</label>
+              <Field
+                type="text"
+                name="اسم_الاب"
+                className="form-control"
+                required
+              />
+              <ErrorMessage name="اسم_الاب" component="div" className="error-message" style={{ fontSize: 20 }} />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="العمر" style={{ fontSize: 20 }}>العمر</label>
+              <Field
+                type="number"
+                name="العمر"
+                className="form-control"
+                required
+                
+              />
+              <ErrorMessage name="العمر" component="div" className="error-message" style={{ fontSize: 20 }} />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="العنوان" style={{ fontSize: 20 }}>العنوان</label>
+              <Field
+                type="text"
+                name="العنوان"
+                className="form-control"
+                required
+              />
+              <ErrorMessage name="العنوان" component="div" className="error-message" style={{ fontSize: 20 }} />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="رقم_الهاتف" style={{ fontSize: 20 }}>رقم الهاتف</label>
+              <Field
+                type="tel"
+                name="رقم_الهاتف"
+                className="form-control"
+                required
+               
+              />
+              <ErrorMessage name="رقم_الهاتف" component="div" className="error-message" style={{ fontSize: 20 }} />
+            </div>
+          </div>
+        </Form>
+      </Formik>
+
+)}
+
+    </div>
+    <button type="submit" className="button" onClick={handleSubmit}>تأكيد</button>
+
     </div>
   );
 };
 
-export default Userinfoconformation;
+export default UserInfoConformation;
